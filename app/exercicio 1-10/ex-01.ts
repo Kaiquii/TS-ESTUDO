3bbac9aebb3ab1fa537e4646b47551f8d8e5169e
Kaiqui <kaiqu@example.com>
2025-11-26 13:28:24 -0300

    estudo

---
+++ app/exercicio 1-10/ex-01.ts
@@ -0,0 +1,8 @@
+/* Crie uma função que retorna a string "Olá, Mundo" */
+
+function dizerOla() {
+  return "Olá, Mundo";
+}
+
+const olaMundo = dizerOla();
+console.log(olaMundo);
---
+++ app/exercicio 1-10/ex-02.ts
@@ -0,0 +1,8 @@
+/* A função recebe um parâmetro do tipo number e retorna o resultado da multiplicação por 2. */
+
+function multiplicacao(valor: number) {
+  return valor * 2;
+}
+
+const valor = multiplicacao(4);
+console.log(valor);
